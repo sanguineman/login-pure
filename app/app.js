84196876edcec5136cbe488ff2f7091c81d20e6e
sanguineman <sanguineman@naver.com>
2021-07-19 00:38:36 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -2,9 +2,11 @@
 
 // 모듈
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+
+const app = express();
 dotenv.config();
+
 // 라우팅
 const home = require("./src/routes/home");
 
@@ -15,6 +17,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({extended: true}));
+
 app.use("/",home); // use -> 미들 웨어를 등록해주는 메소드
 
 module.exports = app;
