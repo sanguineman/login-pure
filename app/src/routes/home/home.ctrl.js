2249a0d9f1a8b6ff0ae86b7650bd162e83e0acac
sanguineman <sanguineman@naver.com>
2021-07-19 01:46:52 +0900

    백엔드 맛보기 최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,16 +5,17 @@ const User = require("../../models/User");
 
 const output = {
   home : (req,res) => {
-    logger.info("GET / 200 '홈 화면으로 이동'");
+    logger.info("GET / 304 '홈 화면으로 이동'");
     res.render("home/index"); // index.ejs 로 이동한다. 
   }, 
+
   login : (req,res)=>{
-    logger.info("GET /login 200 '홈 화면으로 이동'");
+    logger.info("GET /login 304 '홈 화면으로 이동'");
     res.render("home/login"); // login.ejs 로 이동한다.
   },
 
   register: (req,res) => {
-    logger.info("GET /register 200 '홈 화면으로 이동'");
+    logger.info("GET /register 304 '홈 화면으로 이동'");
     res.render("home/register");
   }
 };
@@ -23,28 +24,26 @@ const process = {
   login : async (req,res) => {
     const user = new User(req.body);
     const response = await user.login();
-    if(response.err)
-      logger.error(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200,
+    };
+    log(response, url);
+    return res.status(url.status).json(response);
   },
+
   register: async (req,res)=>{
     const user = new User(req.body);
     const response = await user.register();
-    if(response.err)
-      logger.error(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 409 : 201,
+    };
+    log(response, url);
+    return res.status(url.status).json(response);
   }
 };
 
@@ -53,6 +52,17 @@ module.exports = {
   process,
 };
 
+const log = (response, url) => {
+  if(response.err){
+      logger.error(
+        `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`
+      );
+    } else {
+        logger.info(
+        `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg || ""}`
+      );
+    }
+}
 /*
 key 만 객체에 넣어주었을 때 자동으로 key 와 같은 value를 할당한다.
 {
