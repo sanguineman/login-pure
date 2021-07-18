165629c8478abde0d62a4e604df51672d059b84b
sanguineman <sanguineman@naver.com>
2021-07-18 15:43:32 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -13,15 +13,25 @@ class UserStorage {
 
     return userInfo;
   }
-    static getUsers(...fields){
-      // const users = this.#users;
-      const newUsers = fields.reduce((newUsers, field) => {
-        if(users.hasOwnProperty(field)){
-          newUsers[field] = users[field];
-        }
-        return newUsers;
-      },{});
+  static #getUsers(data, isAll, fields){
+    const users = JSON.parse(data);
+    if(isAll) return users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if(users.hasOwnProperty(field)){
+        newUsers[field] = users[field];
+      }
       return newUsers;
+    },{});
+    return newUsers;
+  }
+    static getUsers(isAll, ...fields){
+      return fs
+      .readFile('./src/databases/users.json')
+      .then((data)=>{
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch(console.error)
+      
     }
 
     static getUserInfo(id){
@@ -33,12 +43,17 @@ class UserStorage {
     }
 
     
-    static save(userInfo){
-      // const users = this.#users;
+    static async save(userInfo){
+      const users = await this.getUsers(true);
+      if(users.id.includes(userInfo.id)){
+        throw "이미 존재하는 아이디입니다.";
+      }
       users.id.push(userInfo.id);
       users.name.push(userInfo.name);
       users.psword.push(userInfo.psword);
-      return { success: true};
+      // 데이터를 추가하지만, 덮어쓰는 방식으로 추가한다. 
+      fs.writeFile('./src/databases/users.json', JSON.stringify(users));
+      return { success : true };
     }
 }
 
